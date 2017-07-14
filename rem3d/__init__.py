615e96ab00622b037a0ddbef643e5b5e5d2c863d
Pritwiraj Moulik <pm2520@columbia.edu>
2017-07-14 05:26:10 -0400

    added documentation support for matlab

---
+++ rem3d/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from .version import version as __version__
+
--- setup.py
+++ setup.py
@@ -9,18 +9,20 @@ metadata = dict(name = 'rem3d',
                 description='a modeling and analysis toolkit for reference Earth datasets and tomographic models.',
                 url='http://www.rem3d.org',
     		author = 'Pritwiraj Moulik',
-                author_email='moulik@ldeo.columbia.edu',
+                author_email='moulik.umd@gmail.com',
                 license='GPL',
                 long_description='REM3D is a Python library for reference Earth datasets and tomographic models.',
     			packages = ['rem3d'],
                 package_data={'rem3d': ['data/input_*/*']},
-    		keywords = ['modeling', 'earth'],
+    		keywords = ['earth-science','earth-observation','earthquake','earth','earthquake-data','geology','geophysics','geophysical-inversions','seismology','seismic-inversion','seismic-waves','seismic-tomography','mineral','geochemistry','geodesy','physics','modeling','modeling-tool','model','geodynamics'],
                 classifiers=[
                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
                 'Programming Language :: Python :: 2.7',
                 'Programming Language :: Python :: 3.4'],
                 )
 
+
+'earth-science','earth-observation','earthquake','earth','earthquake-data','geology','geophysics','geophysical-inversions','seismology','seismic-inversion','seismic-waves','seismic-tomography','mineral','geochemistry','geodesy','physics','modeling','modeling-tool','model','geodynamics'
 # Try to use setuptools in order to check dependencies.
 # if the system does not have setuptools, fall back on
 # distutils.
