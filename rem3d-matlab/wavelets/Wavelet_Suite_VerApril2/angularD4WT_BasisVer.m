c2ec48b44a0272e272d69190a00e2a3ef426bf83
AnantHariharan1996 <anant.hariharan.1996@gmail.com>
2018-04-03 16:44:31 -0400

    better automation

--- rem3d-matlab/wavelets/Wavelet_Suite_VerApril2/angularD4WT_BasisVer.m
+++ rem3d-matlab/wavelets/Wavelet_Suite_VerApril2/angularD4WT_BasisVer.m
@@ -2,8 +2,11 @@ function f=angularD4WT_BasisVer(N,x,Jmax,tipe)
 
 f = zeros(2^N,2^N);
 
+VeloBasisName = ['/home/anant/Software/rem3d/rem3d/files/Precon_Mmaps.N' num2str(N) '.J' num2str(Jmax) '.D4.mat'];
+wletBasisName = ['/home/anant/Software/rem3d/rem3d/files/Precon_WCoeffs.N' num2str(N) '.J' num2str(Jmax) '.D4.mat'];
+
 if tipe == 'inverse'
-    WaveletMaps_DV_Basis = load('/home/anant/Software/rem3d/rem3d/files/Precon_Mmaps.N8.J5.D4.mat');    
+    WaveletMaps_DV_Basis = load(VeloBasisName);    
     for i = 1:2^(2*N)    
         CurrMap = x(i)*full(WaveletMaps_DV_Basis.Me(i).dv);    
         f = f+CurrMap;
@@ -11,7 +14,7 @@ if tipe == 'inverse'
 end
 
 if tipe == 'forward'
-     WaveletCoeffstoMakeDV = load('/home/anant/Software/rem3d/rem3d/files/Precon_WCoeffs.N8.J5.D4.mat');    
+     WaveletCoeffstoMakeDV = load(wletBasisName);    
     for i = 1:2^(2*N)    
         CurrMap = x(i)*full(WaveletCoeffstoMakeDV.Me(i).dv);    
         f = f+CurrMap;
