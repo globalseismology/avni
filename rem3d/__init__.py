72cef939802e07bf240cc94348671f42aee1f913
Pritwiraj Moulik <theprithwi@gmail.com>
2018-05-11 16:10:43 -0400

    New files

--- rem3d/__init__.py
+++ rem3d/__init__.py
@@ -2,3 +2,19 @@ from __future__ import absolute_import
 
 from .version import version as __version__
 
+
+# constant for downloads etc.
+from . import constants
+
+# generic tools library
+from . import tools
+
+# data library
+from . import data
+
+# models library
+from . import models
+
+# plotting library
+from . import plots
+
---
+++ rem3d/constants.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+import scipy.constants
+import ConfigParser
+import pdb    #for the debugger pdb.set_trace()
+import pkgutil
+import StringIO
+
+"""
+download files
+"""
+downloadpage = 'https://maurya.umd.edu/files'
+localfilefolder = 'files'
