08c742fbe9d288d3f75fd30806eae280f54c8468
Pritwiraj Moulik <theprithwi@gmail.com>
2018-05-16 08:40:57 -0400

    removed waveelts for now

--- rem3d-matlab/wavelets/Wavelet_Suite_VerApril2/Calculations/Get_Fractional_RMS.m
+++
@@ -1,7 +0,0 @@
-function [RMS_Ratio] = Get_Fractional_RMS(Model1,Model2,Geographic_Lon,Geographic_Lat,N,Jmax)
-% Gets RMSs for each model and divides.
-RMS_1 = D4wt_Localized_Model_RMS(Model1,Geographic_Lon,Geographic_Lat,N,Jmax);
-RMS_2 = D4wt_Localized_Model_RMS(Model2,Geographic_Lon,Geographic_Lat,N,Jmax);
-RMS_Ratio = RMS_1/RMS_2;
-end
-
--- rem3d-matlab/wavelets/scripts/partial_input_file_generator.m
+++
@@ -1,9 +0,0 @@
-%input file generator
-
-dvs = load('MIT_P08_epix_model_at_PRI_locations.mat')
-
-header1 = '   '
-fid=fopen('mitdvsonly.txt','w');
-fprintf(fid, [ header1 '\n']);
-fprintf(fid, '%f \n', [v]');
-fclose(fid);
