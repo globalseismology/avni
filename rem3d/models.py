a0d316a048d384225c434368fa352c02d6cc5f56
Ross Maguire <romaguir@umich.edu>
2018-05-19 15:20:28 -0400

    created skeleton of reference1D class

--- rem3d/models.py
+++ rem3d/models.py
@@ -15,11 +15,9 @@ from datetime import date  #to give a timestamp to output and compare times
 import pdb    #for the debugger pdb.set_trace()
 from math import pi
 import fortranformat as ff #reading/writing fortran formatted text
+from future.utils import native_str
 
 #####################
-
-def read_model():
-    return model
 
 def readepixfile(filename):
     """Read .epix file format."""
@@ -34,3 +32,55 @@ def readepixfile(filename):
     epixarr=np.genfromtxt(filename, dtype=None,comments="#",names=['lat','lon','pixsize','val'])
 
     return epixarr
+
+Model1D_Attr = np.dtype([
+    (native_str('radius'),np.float),
+    (native_str('rho'),np.float),
+    (native_str('vp'),np.float),
+    (native_str('vph'),np.float),
+    (native_str('vpv'),np.float),
+    (native_str('vs'),np.float),
+    (native_str('vsh'),np.float),
+    (native_str('vsv'),np.float),
+    (native_str('Qmu'),np.float),
+    (native_str('Qkappa'),np.float),
+    (native_str('eta'),np.float)])
+
+class reference1D(object):
+    '''
+    A class for 1D reference Earth models used in tomography
+    '''
+
+    def __init__(self):
+
+        self.__nlayers__ = None
+	self.data = None
+	self.metdata = None
+
+    def read(self,path_to_model,fmt='card'):
+
+        if fmt=='card':
+            modelarr = np.genfromtxt(path_to_model,dtype=None,comments='#',skip_header=3,
+	        names=['radius','rho','vpv','vsv','Qkappa','Qmu','vph','vsh','eta'])
+      
+        else:
+	    raise ValueError('model format ',fmt,' is not currently implemented')
+
+	self.__nlayers__ = len(modelarr['radius'])
+
+	#Do voigt averaging here?
+	vs = np.zeros(len(modelarr['radius']))
+	vp = np.zeros(len(modelarr['radius']))
+
+	self.data = np.empty(self.__nlayers__,dtype=Model1D_Attr)
+	self.data['radius'] = modelarr['radius']
+	self.data['rho'] = modelarr['rho']
+	self.data['vpv'] = modelarr['vpv']
+	self.data['vsv'] = modelarr['vsv']
+	self.data['Qkappa'] = modelarr['Qkappa']
+	self.data['Qmu'] = modelarr['Qmu']
+	self.data['vph'] = modelarr['vph']
+	self.data['vsh'] = modelarr['vsh']
+	self.data['eta'] = modelarr['eta']
+	self.data['vp'] = vp
+	self.data['vs'] = vs
