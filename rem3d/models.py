4dad74f452df82575e5fb1d64a24216a71781e31
Pritwiraj Moulik <theprithwi@gmail.com>
2018-06-06 15:43:16 -0400

    Cleaned up for smaller code

--- rem3d/models.py
+++ rem3d/models.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
-"""This script/module contains routines that are used to analyze/visualize the data sets 
-in the standard REM3D format. 
-Author: Raj Moulik, 2018"""
+"""
+This script/module contains routines that are used to analyze Earth models and files that
+contain them.
+"""
 
 #####################  IMPORT STANDARD MODULES   ######################################   
 
-import sys,os
-import argparse #parsing arguments
-import glob # pattern matching for finding files
+import os
 import numpy as np #for numerical analysis
-from scipy import stats #for stats analysis
-from subprocess import call #for calling unix commands
-from datetime import date  #to give a timestamp to output and compare times
-import pdb    #for the debugger pdb.set_trace()
-from math import pi
-import fortranformat as ff #reading/writing fortran formatted text
 
 #####################
 
@@ -32,16 +25,3 @@ def readepixfile(filename):
 
     return epixarr
     
-def readcards(filename):
-    """Read card deck file format."""
-
-    currentdir=os.getcwd()
-    try: 
-        f = open(filename, 'r')
-    except IOError:
-        print "File ("+filename+") does not exist in the current directory - "+currentdir
-        sys.exit(2)
-            
-    cardsarr=np.genfromtxt(filename, skip_header=3, dtype=None,comments="#",names=['radius','rho','vpv','vsv','qkappa','qmu','vph','vsh','eta'])
-
-    return cardsarr
--- setup.py
+++ setup.py
@@ -20,69 +20,6 @@ if int(npv[1]) < 7:
 
 import numpy.distutils.fcompiler
 
-# get F90 environment variable
-#----------------------------------------
-F90 = os.getenv("F90")
-
-
-# raise error if F90 not defined
-# !! comment out this if statement for manual install !!
-#------------------------------------------------------------
-if F90 == None or F90 == "":
-    l1 = 'REM3D requires environment variable F90 to be set. \n '
-    l2 = 'Please set to one of {"ifort", "gfortran"}'
-    raise RuntimeError( l1 + l2 )
-
-
-# specialize for different compilers
-#------------------------------------------------------------
-if F90 == "ifort":
-    f90_flags = ["-openmp", "-fPIC", "-xHost", "-O3", "-ipo",
-                 "-funroll-loops", "-heap-arrays", "-mcmodel=medium"]
-    omp_lib = ["-liomp5"]
-
-elif F90 == "gfortran":
-    f90_flags = ["-fopenmp", "-fPIC", "-O3", "-fbounds-check",
-                 "-ffixed-line-length-none","-fdec-math"]
-    omp_lib = ["-lgomp"]
-
-elif F90 == "f90":
-    f90_flags = ["-fopenmp", "-fPIC", "-O3", "-library=sunperf","-xopenmp"]
-    omp_lib = [""]
-
-else:
-    l1 = "F90 = " + F90 + ". \n"
-    l2 = "Environment variable F90 not recognized.  \n"
-    raise RuntimeError( l1 + l2 )
-
-
-# for manual install comment out the above section and define
-# the variables f90_flags and omp_lib below
-#------------------------------------------------------------
-#f90_flags =
-#omp_lib =
-
-
-# discover fortran compiler
-#---------------------------------------------------------------
-#cmnd = 'f2py -c --help-fcompiler'
-#fcstr = subprocess.check_output( ["f2py", "-c", "--help-fcompiler"] )
-#ii = fcstr.index('Fortran compilers found:')
-#ff = fcstr.index('Compilers available for this platform, but not found:')
-#fcstr_cut = fcstr[ii:ff].split('\n')
-#fcs = [ fcstr_cut[i].strip() for i in range(1,len(fcstr_cut)-1) ]
-#print fcstr_cut
-#print len(fcstr_cut)
-#print fcs
-
-# setup fortran 90 extension
-#---------------------------------------------------------------------------
-
-f90_dir='rem3d/f2py'
-packagelist=['rem3d']
-for module in os.listdir(f90_dir): packagelist.append('rem3d.f2py.'+module)
-
-
 # write short description
 #--------------------------------------------------------------------------
 description = 'a modeling and analysis toolkit for reference Earth ' + \
@@ -113,13 +50,6 @@ from os.path import join
 from numpy.distutils.core import Extension
 from numpy.distutils.core import setup
  
-extf = [Extension(name='rem3d.'+module,
-                sources = [join(f90_dir,module,f) for f in os.listdir(join(f90_dir,module)) if f.endswith('.f')],
-                extra_f90_compile_args = f90_flags,
-                extra_link_args = omp_lib)
-            for module in os.listdir(f90_dir)]
-
-
 metadata = dict(name = 'rem3d',
                 version=versionstuff['version'],
                 description=description,
@@ -128,10 +58,8 @@ metadata = dict(name = 'rem3d',
                 author = 'Pritwiraj Moulik',
                 author_email='pritwiraj.moulik@gmail.com',
                 license='GPL',
-                packages = packagelist,
-                ext_modules = extf,
-                install_requires=['pandas','fortranformat','joblib','progressbar',
-                'cartopy','netCDF4'],
+                packages = ['rem3d'],
+                install_requires=['requests'],
                 keywords = ['earth-science','earth-observation','earthquake',
                 'earth','earthquake-data','geology','geophysics',
                 'geophysical-inversions','seismology','seismic-inversion',
