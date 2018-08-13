c2f0706a47f2ee9a57d9a5b1837a1f2d8b177337
Pritwiraj Moulik <theprithwi@gmail.com>
2018-08-13 18:21:36 -0400

    added uniquenumpyrow and sanitised_input to tools

--- rem3d/tools.py
+++ rem3d/tools.py
@@ -117,3 +117,47 @@ def readjson(filename,encoding='utf-8'):
     listarray = json.loads(obj_text)
     nparray = np.array(listarray)
     return nparray    
+
+def uniquenumpyrow(a):
+    """Gets the unique rows from a numpy array and the indices. e.g. to get unique lat-lon values"""
+    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
+    _, idx = np.unique(b, return_index=True)
+    unique_a = a[idx]
+    return unique_a,idx
+
+
+def sanitised_input(prompt, type_=None, min_=None, max_=None, range_=None): 
+    """Provide a user prompt with values between min-max or range of values e.g.
+    For specific values:
+    user_input = sanitised_input("Replace(r)/Ignore(i) this datum?", str.lower, range_=('r', 'i')
+    For a range:
+    age = sanitised_input("Enter your age: ", int, range_=xrange(100))
+    """
+    if min_ is not None and max_ is not None and max_ < min_: 
+        raise ValueError("min_ must be less than or equal to max_.") 
+    while True: 
+        ui = raw_input(prompt) 
+        if type_ is not None: 
+            try: 
+                ui = type_(ui) 
+            except ValueError: 
+                print("Input type must be {0}.".format(type_.__name__)) 
+                continue
+        if max_ is not None and ui > max_: 
+            print("Input must be less than or equal to {0}.".format(max_)) 
+        elif min_ is not None and ui < min_: 
+            print("Input must be greater than or equal to {0}.".format(min_)) 
+        elif range_ is not None and ui not in range_: 
+            if isinstance(range_, xrange): 
+                template = "Input must be between {0} and {1}."
+                print(template.format(range_[0],range_[-1])) 
+            else: 
+                template = "Input must be {0}."
+                if len(range_) == 1: 
+                    print(template.format(*range_)) 
+                else: 
+                    print(template.format(" or ".join((", ".join(map(str, 
+                                                                     range_[:-1])), 
+                                                       str(range_[-1]))))) 
+        else: 
+            return ui     
