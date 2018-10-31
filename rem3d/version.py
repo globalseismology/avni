793f46ac6c0431b87ce69d16cc737f27f90e50f3
Pritwiraj Moulik <theprithwi@gmail.com>
2018-10-30 21:03:26 -0400

    Added spline code vbspl and an example file

--- rem3d/version.py
+++ rem3d/version.py
@@ -1,2 +1,2 @@
-version = '0.0.alpha10'
-short_version = '0.0.alpha10'
+version = '0.0.alpha13'
+short_version = '0.0.alpha13'
--- setup.py
+++ setup.py
@@ -20,6 +20,75 @@ if int(npv[0]) != 1:
 if int(npv[1]) < 7:
     raise ImportError( 'REM3D requires Numpy 1.7 or later.' )
 
+import numpy.distutils.fcompiler
+
+# get F90 environment variable
+#----------------------------------------
+F90 = os.getenv("F90")
+
+
+# raise error if F90 not defined
+# !! comment out this if statement for manual install !!
+#------------------------------------------------------------
+if F90 == None or F90 == "":
+    l1 = 'REM3D requires environment variable F90 to be set. \n '
+    l2 = 'Please set to one of {"ifort", "gfortran"}'
+    raise RuntimeError( l1 + l2 )
+
+
+# specialize for different compilers
+#------------------------------------------------------------
+if F90 == "ifort":
+    f90_flags = ["-openmp", "-fPIC", "-xHost", "-O3", "-ipo",
+                 "-funroll-loops", "-heap-arrays", "-mcmodel=medium"]
+    omp_lib = ["-liomp5"]
+
+elif F90 == "gfortran":
+    f90_flags = ["-fopenmp", "-fPIC", "-O3", "-fbounds-check",
+                 "-ffixed-line-length-none"]
+    omp_lib = ["-lgomp"]
+
+elif F90 == "f90":
+    f90_flags = ["-fopenmp", "-fPIC", "-O3", "-library=sunperf","-xopenmp"]
+    omp_lib = [""]
+    
+elif F90 in ["pgfortran", "pgf90", "pgf95"]:
+    f90_flags = ["-mp"]
+    omp_lib = [""]
+
+else:
+    l1 = "F90 = " + F90 + ". \n"
+    l2 = "Environment variable F90 not recognized.  \n"
+    raise RuntimeError( l1 + l2 )
+
+
+# for manual install comment out the above section and define
+# the variables f90_flags and omp_lib below
+#------------------------------------------------------------
+#f90_flags =
+#omp_lib =
+
+
+# discover fortran compiler
+#---------------------------------------------------------------
+#cmnd = 'f2py -c --help-fcompiler'
+#fcstr = subprocess.check_output( ["f2py", "-c", "--help-fcompiler"] )
+#ii = fcstr.index('Fortran compilers found:')
+#ff = fcstr.index('Compilers available for this platform, but not found:')
+#fcstr_cut = fcstr[ii:ff].split('\n')
+#fcs = [ fcstr_cut[i].strip() for i in range(1,len(fcstr_cut)-1) ]
+#print fcstr_cut
+#print len(fcstr_cut)
+#print fcs
+
+# setup fortran 90 extension
+#---------------------------------------------------------------------------
+
+f90_dir='rem3d/f2py'
+packagelist=['rem3d']
+#for module in os.listdir(f90_dir): packagelist.append('rem3d.f2py.'+module)
+
+
 # write short description
 #--------------------------------------------------------------------------
 description = 'a modeling and analysis toolkit for reference Earth ' + \
@@ -28,11 +97,9 @@ description = 'a modeling and analysis toolkit for reference Earth ' + \
 
 # puts the contents of the README file in the variable long_description
 #--------------------------------------------------------------------------
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+with open('README.md') as file:
+    long_description = '\n\n ' + file.read()
+
 
 # call setup
 #--------------------------------------------------------------------------
@@ -46,24 +113,46 @@ versionstuff = dict(
 # distutils.
 # Can only use numpy distutils with distutils as fortran codes 
 # are not compiles otherwise
+#
 # Build the f2py fortran extension
 # --------------------------------
 from os.path import join
 from numpy.distutils.core import Extension
 from numpy.distutils.core import setup
  
+# Use this if you need import rem3d.module for every module folder
+# extf = [Extension(name='rem3d.'+module,
+#                 sources = [join(f90_dir,module,f) for f in os.listdir(join(f90_dir,module)) if f.endswith('.f')],
+#                 extra_f77_compile_args = f90_flags,
+#                 extra_f90_compile_args = f90_flags,
+#                 extra_link_args = omp_lib)
+#             for module in os.listdir(f90_dir)]
+# 
+# Use this if you need a single module for all subroutines import rem3d.f2py
+sourcefiles = []
+for path,dir,filelist in os.walk(join(f90_dir)):
+    for f in filelist:
+        if f.endswith('.f'): sourcefiles.append(join(path,f))
+extf = [Extension(name='rem3d.f2py',
+                sources = sourcefiles,
+                extra_f77_compile_args = f90_flags,
+                extra_f90_compile_args = f90_flags,
+                extra_link_args = omp_lib)]
+
 metadata = dict(name = 'rem3d',
                 version=versionstuff['version'],
                 description=description,
                 long_description = long_description,
-                long_description_content_type='text/markdown',
                 url='http://www.rem3d.org',
                 author = 'Pritwiraj Moulik',
                 author_email='pritwiraj.moulik@gmail.com',
                 license='GPL',
-                packages = ['rem3d'],
-                install_requires=['requests','future'],
-                data_files=[('rem3d', ['README.md'])],
+                packages = packagelist,
+                ext_modules = extf,
+                install_requires=['fortranformat','joblib','progressbar2',
+                'requests','future','msgpack','argparse','configobj'],
+                data_files=[('rem3d', ['README.md']),
+                ('rem3d/config',['rem3d/config/attributes.ini'])],
                 keywords = ['earth-science','earth-observation','earthquake',
                 'earth','earthquake-data','geology','geophysics',
                 'geophysical-inversions','seismology','seismic-inversion',
@@ -73,7 +162,8 @@ metadata = dict(name = 'rem3d',
                 classifiers=[
                 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
                 'Programming Language :: Fortran',
-                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
                 'Intended Audience :: Science/Research',
                 'Topic :: Education',
                 'Natural Language :: English',
