2cd6c0a4fc0e212f346332d3d5fbe5d719b0dafc
Pritwiraj Moulik <theprithwi@gmail.com>
2019-03-08 01:24:38 -0500

    made the changes relevant to reordering arrays to (depth,lat,lon) in ascii2xarray

--- examples/plot_transect.py
+++ examples/plot_transect.py
@@ -44,23 +44,23 @@ def main():
     lat1 = 34.;lng1 = 152.;lat2 = 40.;lng2 = 117.
     delta,azep,azst = ddelazgc(lat1,lng1,lat2,lng2)
     
-    plot1section(lat1,lng1,azep,delta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=arg.elev_exxagerate,dbs_path=arg.dbs_path,width_ratios=[1,2],nelevinter=arg.elev_interval,numevalx=200,numevalz=200,k=1)
+    plot1section(lat1,lng1,azep,delta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=arg.elev_exxagerate,dbs_path=arg.dbs_path,width_ratios=[1,2],nelevinter=arg.elev_interval,outfile='Japan.eps',numevalx=200,numevalz=200,k=1)
     
     # Kermadec
     lat1 = -25.;lng1 = 191.;lat2 = -22.;lng2 = 160.
     delta,azep,azst = ddelazgc(lat1,lng1,lat2,lng2)
     
-    plot1section(lat1,lng1,azep,delta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=arg.elev_exxagerate,dbs_path=arg.dbs_path,width_ratios=[1,2],nelevinter=arg.elev_interval,numevalx=200,numevalz=200,k=1)
+    plot1section(lat1,lng1,azep,delta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=arg.elev_exxagerate,dbs_path=arg.dbs_path,width_ratios=[1,2],nelevinter=arg.elev_interval,outfile='Kermadec.eps',numevalx=200,numevalz=200,k=1)
     
     ###### Example of a 180 degree transect without topography
     print("PLOTTING SECTION 2")
     lat1 = 0.;lng1 = 0.;azimuth = -30.;gcdelta = 180.
-    plot1section(lat1,lng1,azimuth,gcdelta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=0,dbs_path=arg.dbs_path,figuresize=[8,4],width_ratios=[1,4],numevalx=360,numevalz=360,k=1)
+    plot1section(lat1,lng1,azimuth,gcdelta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=0,dbs_path=arg.dbs_path,figuresize=[8,4],width_ratios=[1,4],numevalx=360,numevalz=360,k=1,outfile='transect180.eps')
 
     ###### Example of a 360 degree transect without topography
     print("PLOTTING SECTION 3")
     lat1 = 0.;lng1 = 0.;azimuth = -45.;gcdelta = 360.
-    plot1section(lat1,lng1,azimuth,gcdelta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=0,dbs_path=arg.dbs_path,figuresize=[8,4],width_ratios=[1,4],numevalx=720,numevalz=720,k=1)
+    plot1section(lat1,lng1,azimuth,gcdelta,topo=topo,topotree=topo_tree,modeltree=tomo_tree,model=tomo,parameter=arg.parameter,vmin=arg.lower_bound,vmax=arg.upper_bound,colorlabel='$\delta V_{S} / V_{S}$'+' (%)',vexaggerate=0,dbs_path=arg.dbs_path,figuresize=[8,4],width_ratios=[1,4],numevalx=720,numevalz=720,k=1,outfile='transect360.eps')
         
     return
 
