7d560f5e4da5e77c318ffe274fea5f3f4de51dcf
Pritwiraj Moulik <theprithwi@gmail.com>
2019-03-07 23:23:27 -0500

    made the changes relevant to reordering arrays to (depth,lat,lon) in ascii2xarray

--- rem3d/models.py
+++ rem3d/models.py
@@ -472,7 +472,6 @@ def epix2ascii(model_dir='.',setup_file='setup.cfg',output_dir='.',n_hpar=1,writ
                 lats_new = f[:,0]
                 lons_new = f[:,1]
                 pxs_new = f[:,2]
-                
 
                 #loop through all previous parameterizations
                 for ii in range(len(lats)):
@@ -725,20 +724,30 @@ def ascii2xarray(asciioutput,outfile=None,setup_file='setup.cfg',complevel=9, en
         
         if stru_idx is not None:
             dep = rpar_list[stru_idx]
-            data_array = xr.DataArray(np.zeros((len(dep),len(lon),len(lat))),
-                                      dims = ['depth','longitude','latitude'],
-                                      #dims = ['depth','latitude','longitude'],
-                                      coords=[dep,lon,lat])
+            #data_array = xr.DataArray(np.zeros((len(dep),len(lon),len(lat))),
+            #                          dims = ['depth','longitude','latitude'],
+            #                          coords=[dep,lon,lat])
+            data_array = xr.DataArray(np.zeros((len(dep),len(lat),len(lon))),
+                                      dims = ['depth','latitude','longitude'],
+                                      coords=[dep,lat,lon])
             for i,layer in enumerate(model_dict[variable]['layers']):
+                #TODO reorder data
+                #data_array[i,:,:] = np.reshape(model_dict[variable]['layers'][layer],
+                #                    (len(lon),len(lat)),order='F')
                 data_array[i,:,:] = np.reshape(model_dict[variable]['layers'][layer],
-                                    (len(lon),len(lat)),order='F')
+                                    (len(lat),len(lon)),order='C')
             ds[variable] = data_array
         else:
-            data_array = xr.DataArray(np.zeros((len(lon),len(lat))),
-                                      dims = ['longitude','latitude'],
-                                      coords = [lon,lat])
+            #data_array = xr.DataArray(np.zeros((len(lon),len(lat))),
+            #                          dims = ['longitude','latitude'],
+            #                          coords = [lon,lat])
+            data_array = xr.DataArray(np.zeros((len(lat),len(lon))),
+                                      dims = ['latitude','longitude'],
+                                      coords = [lat,lon])
+            #data_array[:,:] = np.reshape(model_dict[variable]['layers'][0],
+            #                        (len(lon),len(lat)),order='F')
             data_array[:,:] = np.reshape(model_dict[variable]['layers'][0],
-                                    (len(lon),len(lat)),order='F')
+                                    (len(lat),len(lon)),order='C')
             ds[variable] = data_array
 
     #add attributes
