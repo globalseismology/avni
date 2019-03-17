2eda0584958b5cce63994814438c3c7231088790
Pritwiraj Moulik <theprithwi@gmail.com>
2019-03-17 00:56:07 -0400

    created getplanetconstants in tools to read Earth constants by default

--- rem3d/__init__.py
+++ rem3d/__init__.py
@@ -21,3 +21,7 @@ from .version import version as __version__
 __path__ = pkgutil.extend_path(__path__, __name__)
 for importer, modname, ispkg in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
     __import__(modname)
+    
+# initialize the constants for planet Earth by default
+configdir = tools.get_configdir()
+tools.getplanetconstants(constants.planetpreferred,configdir+'/'+constants.planetconstants)
