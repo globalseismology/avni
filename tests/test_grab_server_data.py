7e01a369f5368749764b48ff637cb9c397ce5850
Pritwiraj Moulik <theprithwi@gmail.com>
2019-04-28 03:13:56 -0400

    more travis and codecov testing

--- tests/test_grab_server_data.py
+++ tests/test_grab_server_data.py
@@ -5,24 +5,7 @@ import argparse #parsing arguments
 
 ########################### IMPORT REM3D MODULES   #####################################
 import rem3d
-import pytest
 #########################################################
-@pytest.fixture
-def test_download(file,folder):
-    """download a file"""
-    rem3d.data.update_file(file,folder)
-
-def main():
-    parser = argparse.ArgumentParser(description='grab a file from the REM3D server')
-    parser.add_argument('-i', '--file', type=str, default='S362ANI+M.vs.5.epix',
-        help='file from REM3D server')
-    parser.add_argument('-f', '--folder', type=str,default='.',
-        help='output folder')
-    arg = parser.parse_args()
-
-    # update the file from the server
-    rem3d.data.update_file(arg.file,arg.folder)
-    return
-
-if __name__== "__main__":
-    main()
+file = 'S362ANI+M.vs.5.epix'
+folder = '.'
+rem3d.data.update_file(file,folder)
