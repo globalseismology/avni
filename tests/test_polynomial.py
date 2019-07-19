e0b566a907a816db5c4abda26627744fd386df7e
Pritwiraj Moulik <theprithwi@gmail.com>
2019-07-19 00:42:48 -0400

    minor adjustments

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -11,7 +11,7 @@ assert vercof[0] == 1.,'Check that the value of a CONSTANT basis is 1 within the
 vercof,dvercof = rem3d.tools.bases.eval_polynomial(rnorm-150.,radius_range=[rnorm-35.,rnorm-20.] ,rnorm=rnorm,types=['CONSTANT','LINEAR'])
 assert np.all(vercof==0.), 'Check that the value of a CONSTANT basis is 0 outside the domain'
 
-vercof,dvercof = rem3d.tools.bases.eval_polynomial(rnorm-150.,radius_range=[rnorm-200.,rnorm-100.] ,rnorm=rnorm,types=['TOP','BOTTOM'])
+vercof,dvercof = rem3d.tools.bases.eval_polynomial([rnorm-150., rnorm-180.],radius_range=[rnorm-200.,rnorm-100.] ,rnorm=rnorm,types=['TOP','BOTTOM'])
 top= bottom = 3.45 #m/s
-coeff = np.array([[top,bottom]])
-assert np.isclose(sum(coeff*vercof),top), 'evaluation of TOP/BOTTOM should give constant value anywhere in the domain'
+coeff = np.array([[top,bottom]]).T
+assert np.all(np.isclose(np.dot(vercof,coeff),top)), 'evaluation of TOP/BOTTOM should give constant value anywhere in the domain'
